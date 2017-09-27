6a9db061d11c9fb894a0d6bcb5ad0aea545d9ea4
Builder's Brewery <buildersbrewery@users.noreply.github.com>
2017-09-27 23:30:23 +0200

    Update __init__.py

--- sublimetext/LSL/src/__init__.py
+++ sublimetext/LSL/src/__init__.py
@@ -1,6 +1,7 @@
-__pkg_name__ = 'LSL'
+#!/usr/bin/env python
+# coding: utf-8
 
-from .pc_events_notifier import *
+
 from .sublimelinter_contrib_lslint import *
 from .tooltips import *
 from .window_commands import *
